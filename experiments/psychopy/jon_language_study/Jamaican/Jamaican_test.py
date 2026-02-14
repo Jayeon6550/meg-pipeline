68026e4865a9a9199b81400f8bd04292154f07ed
Hadi Zaatiti <hadizaatiti@gmail.com>
2026-02-14 16:42:52 +0400

    update files

--- experiments/psychopy/jon_language_study/Jamaican/Jamaican_test.py
+++ experiments/psychopy/jon_language_study/Jamaican/Jamaican_test.py
@@ -49,7 +49,7 @@ SCREEN_NUMBER = 2
 #Try 1 or 2 as screen_number
 #SCREEN_NUMBER = 1
 
-trialList = data.importConditions('jc_test1.csv')
+trialList = data.importConditions('test2.csv')
 
 #mon = monitors.Monitor('BenQ24', width=53, distance=100)
 #port = parallel.ParallelPort(address=0xD010)
@@ -62,9 +62,9 @@ stimuliColor = 'gold' #rgb(255, 215, 0)
 stimuliUnits = 'deg'
 stimuliSize = 2
 instructionSize = 1
-wordOn = 36 #300ms #42 #350ms
-wordOff = 24 #200ms #24 #200ms
-lastWordOn = 120  #1000 #132  #1100
+wordOn =  42 #36 #300ms #42 #350ms
+wordOff = 24 #24 #200ms #24 #200ms
+lastWordOn = 132  #120  #1000 #132  #1100
 
 boxHeight = stimuliSize + 1.5
 boxWidth = 16
