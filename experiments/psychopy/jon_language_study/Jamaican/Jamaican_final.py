15fc147826f53da569fbcebc700304761965fefa
Hadi Zaatiti <hadizaatiti@gmail.com>
2026-02-16 10:23:11 +0400

    update files

--- experiments/psychopy/jon_language_study/Jamaican/Jamaican_final.py
+++ experiments/psychopy/jon_language_study/Jamaican/Jamaican_final.py
@@ -62,9 +62,9 @@ stimuliColor = 'gold' #rgb(255, 215, 0)
 stimuliUnits = 'deg'
 stimuliSize = 2
 instructionSize = 1
-wordOn = 36 #300ms #42 #350ms
-wordOff = 24 #200ms #24 #200ms
-lastWordOn = 120  #1000 #132  #1100
+wordOn = 42 #350ms
+wordOff = 24 #200ms
+lastWordOn = 132 #1100
 
 boxHeight = stimuliSize + 1.5
 boxWidth = 16
